e7dbe6f952c9a0392bd4b80c43526d8295be3a3c
April Copes <antdevelopment1@gmail.com>
2018-11-07 21:50:51 -0500

    insert statements for database

---
+++ seed.sql
@@ -0,0 +1,51 @@
+-- Data / Insert statements of goals and info about each user.
+
+-- Insert statements for info about the user 
+INSERT INTO users(name, age, gender) VALUES('Batman', 50, 'MALE');
+INSERT INTO users(name, age, gender) VALUES('April Copes', 27, 'FEMALE');
+INSERT INTO users(name, age, gender) VALUES('Justin Timberlake', 40, 'MALE');
+INSERT INTO users(name, age, gender) VALUES('Lauren Wilkerson', 27, 'FEMALE');
+INSERT INTO users(name, age, gender) VALUES('Captain America', 120, 'MALE');
+
+-- Insert statements about goals from each user
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Fight crime.', true, 100000, 1);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Save the world.', false, 400000, 5);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Start learning how to code like a badass.', true, 100000, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Learn the higher levels of math and physics.', false, 349000, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Own a coffe shop.', false, 283, 4);
+
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Save Gothem City.', false, 500000, 1);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Play in a metal band.', false, 200, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Sing', true, 0, 3);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Cook and grow organic food.', true, 300, 4);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Study classical guitar.', false, 50, 2);
+
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Lower crash statistics across the U.S.', false, 820000, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Dance', true, 0, 3);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Learn archery', false, 100000, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Practice archery', false, 20000, 4);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Play with cool gadets', true, 0, 1);
+
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Learn advanced robotics.', false, 3000, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Buy a state of the art drumset', false, 380, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Hang out with Batman', true, 0, 5);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Learn how to code', true, 200, 4);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Rescue all puppies and kittens from harms way.', false, 109200, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Study metal and blues on electric guitar.', true, 0, 2);
+
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Go for a walk in the park', false, 3, 4);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Marry Jessica Biel', true, 900, 3);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Learn 4eva!!!!', true, 0, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Teach everything I know to the next generation.', true, 10, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Make gourmet coffee', true, 100000, 4);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Ask Lauren where all my lost items are.', true, 18300, 2);
+INSERT INTO goals(goal, completed, time_frame, user_id) VALUES('Know the hidden places of where April forgets where she puts things', true, 0, 4);
+
+
+
+
+
+
+
+
+
